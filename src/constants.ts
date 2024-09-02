61bd4618e6788b2fec7f0440c44696f5b2937800
Bektur Razzakov <brzzkv@gmail.com>
2024-09-02 12:38:24 +0600

    Created at date for applications

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,14 @@
 export const apiUrl = 'https://ticketup.skynet.kg:3000/api/';
+
+export const formatDate = (date: string, showTime?: boolean) => {
+  const newDate = new Date(date);
+  const pad = (num: number, size: number) => num.toString().padStart(size, '0');
+  
+  const year = newDate.getFullYear();
+  const month = pad(newDate.getMonth() + 1, 2);
+  const day = pad(newDate.getDate(), 2);
+  const hours = pad(newDate.getHours(), 2);
+  const minutes = pad(newDate.getMinutes(), 2);
+  
+  return `${day}.${month}.${year}${showTime ? ` ${hours}:${minutes}` : ''}`;
+};
--- src/containers/myApplications/MyApplications.tsx
+++ src/containers/myApplications/MyApplications.tsx
@@ -5,6 +5,7 @@ import axiosApi from "../../axiosApi";
 import {useAppSelector} from "../../app/hooks";
 import {useNavigate} from "react-router-dom";
 import './myApplications.css';
+import {formatDate} from "../../constants";
 
 export interface IAppSate {
   id: number,
@@ -37,85 +38,126 @@ const MyApplications = () => {
   const [application, setApplication] = useState<IAppSate | null>(null);
   const [applicationPopup, setApplicationPopup] = useState('');
   const [applicationsLoading, setApplicationsLoading] = useState(false);
-
+  
   const getApps = async () => {
     try {
       setApplicationsLoading(true);
       const apps = await axiosApi('mazay/');
       setApplications(apps.data);
       setApplicationsLoading(false);
+    } catch (e) {
+      console.log(e);
+      setApplicationsLoading(false);
     }
-     catch (e) {
-       console.log(e);
-       setApplicationsLoading(false);
-     }
   };
-
+  
   useEffect(() => {
     if (!userToken) {
       navigate('/sign-in');
     }
     void getApps();
   }, [navigate, userToken]);
-
+  
   const toggleAppPopup = (value: string) => {
     setApplicationPopup(value);
     if (value === 'closed') setApplication(null);
   };
-
+  
   return (
-    <Paper className="my-applications-container" elevation={4}>
-      <ApplicationPopup togglePopup={toggleAppPopup} popup={applicationPopup}>
-        <Typography variant="body1" className="application-popup-title">Информация о заявке</Typography>
-        <Paper elevation={1} className="application-item-content">
-          <span className="application-item-key-name">hydra abbon ls: </span>
-          <span className="application-item-key-value">{application?.hydra_abbon_ls || '-'}</span>
+    <Paper
+      className='my-applications-container'
+      elevation={4}
+    >
+      <ApplicationPopup
+        togglePopup={toggleAppPopup}
+        popup={applicationPopup}
+      >
+        <Typography
+          variant='body1'
+          className='application-popup-title'
+        >Информация о заявке</Typography>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>hydra abbon ls: </span>
+          <span className='application-item-key-value'>{application?.hydra_abbon_ls || '-'}</span>
         </Paper>
-        <Paper elevation={1} className="application-item-content">
-          <span className="application-item-key-name">Гидра договор: </span>
-          <span className="application-item-key-value">{application?.hydra_dogovor || '-'}</span>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>Гидра договор: </span>
+          <span className='application-item-key-value'>{application?.hydra_dogovor || '-'}</span>
         </Paper>
-        <Paper elevation={1} className="application-item-content">
-          <span className="application-item-key-name">Адрес: </span>
-          <span className="application-item-key-value">{application?.hydra_address || '-'}</span>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>Адрес: </span>
+          <span className='application-item-key-value'>{application?.hydra_address || '-'}</span>
         </Paper>
-        <Paper elevation={1} className="application-item-content">
-          <span className="application-item-key-name">Статус: </span>
-          <span className="application-item-key-value">{application?.status || '-'}</span>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>Статус: </span>
+          <span className='application-item-key-value'>{application?.status || '-'}</span>
         </Paper>
-        <Paper elevation={1} className="application-item-content">
-          <span className="application-item-key-name">ID: </span>
-          <span className="application-item-key-value">{application?.id || '-'}</span>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>ID: </span>
+          <span className='application-item-key-value'>{application?.id || '-'}</span>
+        </Paper>
+        <Paper
+          elevation={1}
+          className='application-item-content'
+        >
+          <span className='application-item-key-name'>Дата создания: </span>
+          <span className='application-item-key-value'>{application?.created_at ? formatDate(application?.created_at, true) : '-'}</span>
         </Paper>
       </ApplicationPopup>
       {
         applicationsLoading ?
-          <div className="applications-loading">
+          <div className='applications-loading'>
             <CircularProgress/>
           </div> :
           !applications.length ?
-            <Typography variant="h5" className="no-applications">Нет заявок</Typography> :
-            <div className="application-items">{
+            <Typography
+              variant='h5'
+              className='no-applications'
+            >Нет заявок</Typography> :
+            <div className='application-items'>{
               applications.map((app, i) => (
-                <Paper className="application-item" key={i} onClick={() => {
-                  setApplication(app);
-                  toggleAppPopup('opened');
-                }}>
-                  <div className="application-item-content">
-                    <span className="application-item-key-name">hydra abbon ls: </span>
-                    <span className="application-item-key-value">{app.hydra_abbon_ls || '-'}</span>
+                <Paper
+                  className='application-item'
+                  key={i}
+                  onClick={() => {
+                    setApplication(app);
+                    toggleAppPopup('opened');
+                  }}
+                >
+                  <div className='application-item-content'>
+                    <span className='application-item-key-name'>hydra abbon ls: </span>
+                    <span className='application-item-key-value'>{app.hydra_abbon_ls || '-'}</span>
                   </div>
-                  <div className="application-item-content">
-                    <span className="application-item-key-name">Гидра договор: </span>
-                    <span className="application-item-key-value">{app.hydra_dogovor || '-'}</span>
+                  <div className='application-item-content'>
+                    <span className='application-item-key-name'>Гидра договор: </span>
+                    <span className='application-item-key-value'>{app.hydra_dogovor || '-'}</span>
                   </div>
-                  <div className="application-item-content">
-                    <span className="application-item-key-name">Адрес: </span>
-                    <span className="application-item-key-value">{app.hydra_address || '-'}</span>
+                  <div className='application-item-content'>
+                    <span className='application-item-key-name'>Адрес: </span>
+                    <span className='application-item-key-value'>{app.hydra_address || '-'}</span>
                   </div>
-                  <div className="application-item-content">
-                    <span className="application-item-key-name">Статус: </span>
-                    <span className="application-item-key-value">{app.status || '-'}</span>
+                  <div className='application-item-content'>
+                    <span className='application-item-key-name'>Статус: </span>
+                    <span className='application-item-key-value'>{app.status || '-'}</span>
+                  </div>
+                  <div className='application-item-content'>
+                    <span className='application-item-key-name'>Дата создания: </span>
+                    <span className='application-item-key-value'>{app?.created_at ? formatDate(app.created_at, true) : '-'}</span>
                   </div>
                 </Paper>
               ))
